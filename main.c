4796f9f35d395b9a9fccd83ce761b2432d323e69
Sam Faull <samz5991@hotmail.co.uk>
2018-08-21 20:20:46 +0100

    Correctly implemented XBM splash screen array

--- main.c
+++ main.c
@@ -29,12 +29,13 @@ int16_t main(void)
     //wm8510_config(); //configure WM8510    
     //sram_spi_init(); // initialise spi peripheral
     //sram_init(seq); //initialise 23LC1024 in sequential mode
-    //InitI2C1(); //initialise i2c peripheral
+    InitI2C1(); //initialise i2c peripheral
     oled_init(); //initialise SSD1306 OLED display
     //navpanel_init();
     
     //selftest();
     printf("Ready \n");
+    
     while(1)
     {
         // check for button presses and encoder rotations
