1efbd8c9af97592188f2bfe80459a0eebce12f9e
Sam Faull <samz5991@hotmail.co.uk>
2018-08-24 19:22:39 +0100

    Fixed EEPROM driver by adding more IdleI2C calls. Tested and working

--- eeprom.c
+++ eeprom.c
@@ -7,7 +7,7 @@
 #include <xc.h>
 #include "i2c.h"
 #include "eeprom.h"
-
+#include <stdio.h>
 
 void eeprom_write_byte(unsigned int address, unsigned char data)
 {    
@@ -15,24 +15,36 @@ void eeprom_write_byte(unsigned int address, unsigned char data)
     StartI2C1();
     WaitFlag();
     
+    IdleI2C1();
+    
 	MasterWriteI2C1(EEPROM_WRITE);			//Write Control byte
 	WaitFlag();
     WaitForACK();
     
+    IdleI2C1();
+    
     MasterWriteI2C1(address >> 8);				//Write High Address
 	WaitFlag();
     WaitForACK();
+    
+    IdleI2C1();
 	
 	MasterWriteI2C1(address);				//Write Low Address
 	WaitFlag();
     WaitForACK();
+    
+    IdleI2C1();
 
 	MasterWriteI2C1(data);					//Write Data#
     WaitFlag();
     WaitForACK();
     
 	IdleI2C1();
-	StopI2C1();						//Initiate Stop Condition
+	StopI2C1();						//Initiate Stop Condition  
+    
+    WaitFlag();
+    WaitForACK();
+    IdleI2C1();	//Wait to complete  
 }
 
 unsigned char eeprom_read_byte(unsigned int address)
@@ -41,32 +53,50 @@ unsigned char eeprom_read_byte(unsigned int address)
     StartI2C1();
     WaitFlag();
     
+    IdleI2C1();
+
 	MasterWriteI2C1(EEPROM_WRITE);		//Write Control Byte
 	WaitFlag();
     WaitForACK();
+    
+    IdleI2C1();
     
 	MasterWriteI2C1(address >> 8);			//Write start address (high)
 	WaitFlag();
     WaitForACK();
     
+    IdleI2C1();
+    
     MasterWriteI2C1(address);			//Write start address (low))
 	WaitFlag();
     WaitForACK();
 
+    IdleI2C1();
+    
     ResetVariables_I2C1();           //send start condition
 	StartI2C1();
     WaitFlag();
     
+    IdleI2C1();
+
 	MasterWriteI2C1(EEPROM_READ);	//Write control byte for read
 	WaitFlag();
     WaitForACK();
+    
+    IdleI2C1();
 
 	unsigned char data = MasterReadI2C1();
+    
+    IdleI2C1();
     
 	NotAckI2C1();				//Send NAck
     
     IdleI2C1();
 	StopI2C1();						//Initiate Stop Condition
+    
+    WaitFlag();
+    WaitForACK();
+    IdleI2C1();	//Wait to complete  
     
     return data;
 }
@@ -95,6 +125,10 @@ void eeprom_write_seq(unsigned int address, unsigned char *data)
     
 	IdleI2C1();
 	StopI2C1();						//Initiate Stop Condition
+    
+    WaitFlag();
+    WaitForACK();
+    IdleI2C1();	//Wait to complete  
 }
 
 
@@ -131,4 +165,8 @@ void eeprom_read_seq(unsigned int address, unsigned char *rdptr, unsigned char l
     
     IdleI2C1();
 	StopI2C1();						//Initiate Stop Condition
+    
+    WaitFlag();
+    WaitForACK();
+    IdleI2C1();	//Wait to complete  
 }
