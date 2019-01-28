eaa905c3069b4acf4e4e2406e7140940ad667750
Sam Faull <samz5991@hotmail.co.uk>
2019-01-28 20:19:41 +0000

    Implemented scrollbar for menu system and generally spiced up the layout a little. Looks cleaner now

--- menu.c
+++ menu.c
@@ -15,19 +15,45 @@
 void menu_draw(menu_t *menu)
 {
     int i;
-    const uint8_t temp[3] = {15, 31, 47};
+    const uint8_t temp[3] = {24, 40, 56};
     
     oled_clear();
     oled_println(menu->Heading);
+    //oled_draw_rect(0,0,LCDWIDTH-2,(LCDHEIGHT/(MAX_ITEMS+1)-2));
+    oled_draw_line(0, (LCDHEIGHT/(MAX_ITEMS+1)-2), LCDWIDTH-3, (LCDHEIGHT/(MAX_ITEMS+1)-2) );
+    // for each item in the list, print the content on a new line
     for (i=0; i<MAX_ITEMS; i++)
     {
-        char item = menu->Item[menu->FirstDisplayedItem + i];
-        //char item[30];
-        //strcpy(item, menu->Item[menu->FirstDisplayedItem + i]);
+        oled_write_string("   ");
         oled_println(menu->Item[menu->FirstDisplayedItem + i]);
     }
 
-    oled_draw_rect(0,temp[menu->SelectedPosition],LCDWIDTH-2,LCDHEIGHT/(MAX_ITEMS+1)+1);
+    // draw the item selction rectangle
+    //oled_draw_rect(0,temp[menu->SelectedPosition],LCDWIDTH-7,LCDHEIGHT/(MAX_ITEMS+1)+1);
+    oled_draw_circle(3, temp[menu->SelectedPosition], 3);
+    /* ---- vertical scroll bar ---- */    
+    int itemCount = menu_getCount(menu);
+    
+    // only draw a scroll bar if number of items in menu exceeds number we can display
+    if (itemCount > MAX_ITEMS)
+    {
+        const uint8_t x = 122;
+        const uint8_t y = 14;
+        const uint8_t width = 4;
+        const uint8_t height = 50;
+    
+        // do the calculations to get the right sized scroll bar/marker
+        int h2 = (height * MAX_ITEMS)/itemCount ;
+        int a = itemCount - MAX_ITEMS - 1;
+        float ratio = (float)(menu->FirstDisplayedItem)/(float)a;
+        int offset = (float)(height-h2)*ratio;
+        
+        // draw the scrollbar outline
+        oled_draw_rect(x, y, width, height);
+        // draw the actual bar/marker
+        oled_fill_rect(x, y+offset, width, h2);
+    }
+    
     oled_update();
 }
 
